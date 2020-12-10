68ff125d645bbeb61ea52430d8d6a82fd8695786
Himani0406 <singh.himani0406@gmail.com>
2020-12-10 13:43:04 +0530

    Update bookingConfirmed.php

--- php/bookingConfirmed.php
+++ php/bookingConfirmed.php
@@ -1,5 +1,5 @@
 <?php 
-  include('userServer.php') 
+  include('userServer.php'); 
   if (!isset($_SESSION['email'])) {
   	$_SESSION['msg'] = "You must log in first";
   	header('location: login.php');
